606c0749fa486dadfe2e38eb87825e2f77f86fb0
peterly2609 <91913673+peterly2609@users.noreply.github.com>
2022-04-27 19:41:38 -0500

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -4,11 +4,11 @@ import time
 while True:
 
     os.chdir('C:/Program Files/Wireshark')
-    os.system('tshark -i Wi-Fi -c 2 -T json > E:/output.json')      # Runs the specified tshark command.
-    os.chdir('E:/')
+    os.system('tshark -i Wi-Fi -c 2 -T json -e eth.src -e frame.time > C:/Users/peter/Desktop/CM_UI/src/assets/rawOutput.json')      # Runs the specified tshark command.
+    os.chdir('C:/Users/peter/Desktop/CM_UI/src/assets/')
 
-    f = open('output2.json', 'rt')
-    fout = open('outputClean.json', 'wt')
+    f = open('rawOutput.json', 'rt')
+    fout = open('output2.json', 'wt')
     for line in f:
         fout.write(line.replace('.', '_'))
     f.close()
