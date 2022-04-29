9e1dc36bb97ddda2889e0abc665e00ce01169d60
Aaradical <55465428+Aaradical@users.noreply.github.com>
2022-04-29 12:24:33 -0500

    Final code

--- fingerprint.py
+++ fingerprint.py
@@ -1,4 +1,3 @@
-import socket
 import hashlib
 from getmac import get_mac_address as gma  # Gets MAC address.
 import pyodbc
@@ -9,7 +8,7 @@ from pathlib import Path
 import datetime
 
 
-# Grabs a device's motherboard serial number. Works with Windows & Linux distributions.
+# Grabs a device's motherboard serial number. Works with Windows, Linux, & Darwin distributions.
 def getMachine_addr():
     global command
     os_type = sys.platform.lower()
@@ -27,22 +26,8 @@ serial = getMachine_addr()
 serialNo = serial.replace("SerialNumber", "")
 print(serialNo)
 
-# Returns IP address.
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(0)
-try:
-    # doesn't even have to be reachable.
-    s.connect(('10.255.255.255', 1))
-    IP = s.getsockname()[0]
-except Exception:
-    IP = '127.0.0.1'
-finally:
-    s.close()
-
-print(IP)
-print(gma())  # Gets MAC Address.
-
-# Makes MAC readable.
+# Gets MAC Address & makes it readable.
+print(gma())
 macString = (gma().replace(':', ''))
 
 # Creates SHA256 fingerprint from Serial Number and MAC address.
@@ -52,40 +37,43 @@ print(fingerprint)
 readableFingerprint = fingerprint.hexdigest()
 print(readableFingerprint)
 
-# Creates date & time for log entry.
+# Creates date & time for log entry. Format is the SQL datetime format.
 now = datetime.datetime.now()
 formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
 # Increments every 60 seconds.
 while True:
-
     # Creates date & time for log entry.
     now = datetime.datetime.now()
     formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
-    # Pushes data to logging DB.
-    conn = pyodbc.connect('Driver={SQL Server};'
-                          'Server=10.20.150.205;'     # Name of your SQL Server.
-                          'UID=logadmin;'
-                          'PWD=abc123;'
+    # Connects to logging DB.
+    conn = pyodbc.connect('Driver={SQL Server};'    # Sets the SQL Driver, such as SQL Server or MySQL.
+                          'Server=10.20.122.67;'  # Name/IP of your SQL Server.
+                          'UID=SecLogAdmin;'  # SQL authentication username
+                          'PWD=abc123;'  # SQL authentication password.
                           'Database=logs;'  # Name of DB in server.
-                          'Trusted_Connection=no;')    # Only used for Windows Auth. for other, use uid/pwd.
+                          'Trusted_Connection=no;')  # Only used for Windows Auth. for other, use uid/pwd.
 
+    # Uses an SQL query to insert fingerprint, mac address, serial number,& detected time into the logging DB.
     cursor = conn.cursor()
     cursor.execute('INSERT INTO Devices VALUES (?, ?, ?, ?)', (readableFingerprint, gma(), serialNo, formatted_date))
     conn.commit()
 
-    # Selects unique values.
+    # Selects only the MOST RECENT values of each device in the logging DB. Then outputs to a json file.
     cursor.execute("SELECT [fingerprint],[macAddress],[serialNum],MAX([timeLastSeen]) as timeLastSeen "
                    "FROM [logs].[dbo].[Devices] GROUP BY [fingerprint], [macAddress], [serialNum]FOR JSON AUTO")
 
-    # Outputs JSON object.
+    # Parses the JSON created above.
     for row in cursor:
         print(row)
 
+        # Stores the results into the website asset folder.
+        # Normally this code should push to a back-end server, where the json is then called on by the front-end site.
+        # However, for this project, we are running the website locally from a single device.
         data_folder = Path("C:/Users/peter/Desktop/CM_UI/src/assets")
         f = open(data_folder / "devices.json", "w")
         f.write(row[0])
         f.close()
 
-    time.sleep(60)      # Script runs every 60 seconds.
+    time.sleep(60)  # Script runs every 60 seconds.
